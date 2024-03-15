5b796e6acaf1f79e27b676fc163b1f886f839d27
Ernestlph <peyhwo3@gmail.com>
2024-03-16 01:33:56 +0800

    Mostly Done

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ bot = commands.Bot(command_prefix='!', intents=intents, case_insensitive=True)
 
 
 # Load token
-with open("token/token.txt", "r") as token_file:
+with open("Token/token.txt", "r") as token_file:
     TOKEN = token_file.read().strip()
 
 
@@ -90,9 +90,9 @@ async def Level1(ctx):
         user_states[ctx.author.id]["state"] = UserState.CHOOSING_LESSON
         user_states[ctx.author.id]["chosen_level"] = "Level1"
         if user_states[ctx.author.id]["chosen_language"] == "Japanese":
-            await ctx.send('Starting at the base with Japanese, I see...Now choose your lesson with !Lesson01 through !Lesson30.')
+            await ctx.send('Starting at the base with Japanese, I see...Now choose your lesson with !Lesson 1 through !Lesson 30.')
         if user_states[ctx.author.id]["chosen_language"] == "Mandarin":
-            await ctx.send('Starting at the base with Mandarin, I see...Now choose your lesson with !Lesson01 through !Lesson30.')
+            await ctx.send('Starting at the base with Mandarin, I see...Now choose your lesson with !Lesson 1 through !Lesson 30.')
     else:
         await ctx.send("I'm afraid this command is out of context right now.")
 
@@ -102,9 +102,9 @@ async def Level2(ctx):
         user_states[ctx.author.id]["state"] = UserState.CHOOSING_LESSON
         user_states[ctx.author.id]["chosen_level"] = "Level2"
         if user_states[ctx.author.id]["chosen_language"] == "Japanese":
-            await ctx.send('Intermediate with Japanese, I see...Now choose your lesson with !Lesson01 through !Lesson30.')
+            await ctx.send('Intermediate with Japanese, I see...Now choose your lesson with !Lesson 1 through !Lesson 30.')
         if user_states[ctx.author.id]["chosen_language"] == "Mandarin":
-            await ctx.send('Intermediate with Mandarin, I see...Now choose your lesson with !Lesson01 through !Lesson30.')
+            await ctx.send('Intermediate with Mandarin, I see...Now choose your lesson with !Lesson 1 through !Lesson 30.')
     else:
         await ctx.send("I'm afraid this command is out of context right now.")
 
@@ -114,9 +114,9 @@ async def Level3(ctx):
         user_states[ctx.author.id]["state"] = UserState.CHOOSING_LESSON
         user_states[ctx.author.id]["chosen_level"] = "Level3"
         if user_states[ctx.author.id]["chosen_language"] == "Japanese":
-            await ctx.send('Advanced with Japanese, I see...Now choose your lesson with !Lesson01 through !Lesson30.')
+            await ctx.send('Advanced with Japanese, I see...Now choose your lesson with !Lesson 1 through !Lesson 30.')
         if user_states[ctx.author.id]["chosen_language"] == "Mandarin":
-            await ctx.send('Advanced with Mandarin, I see...Now choose your lesson with !Lesson01 through !Lesson30.')
+            await ctx.send('Advanced with Mandarin, I see...Now choose your lesson with !Lesson 1 through !Lesson 30.')
     else:
         await ctx.send("I'm afraid this command is out of context right now.")
 
@@ -130,7 +130,7 @@ async def Level4(ctx):
             user_states[ctx.author.id]["state"] = UserState.CHOOSING_LEVEL
             user_states[ctx.author.id]["chosen_level"] = None
         if user_states[ctx.author.id]["chosen_language"] == "Mandarin":
-            await ctx.send('Advanced with Mandarin, I see...Now choose your lesson with !Lesson01 through !Lesson30.')
+            await ctx.send('Advanced with Mandarin, I see...Now choose your lesson with !Lesson 1 through !Lesson 30.')
     else:
         await ctx.send("I'm afraid this command is out of context right now.")
 
@@ -144,7 +144,7 @@ async def Level5(ctx):
             user_states[ctx.author.id]["state"] = UserState.CHOOSING_LEVEL
             user_states[ctx.author.id]["chosen_level"] = None
         if user_states[ctx.author.id]["chosen_language"] == "Mandarin":
-            await ctx.send('Advanced with Mandarin, I see...Now choose your lesson with !Lesson01 through !Lesson30.')
+            await ctx.send('Advanced with Mandarin, I see...Now choose your lesson with !Lesson 1 through !Lesson 30.')
     else:
         await ctx.send("I'm afraid this command is out of context right now.")
 
@@ -177,134 +177,19 @@ async def play_lesson(ctx, lesson_number: int):
     voice_channel.play(audio_source)
     free_bot_task = bot.loop.create_task(free_bot())
 
-@bot.command(name='Lesson01')
-async def lesson01(ctx):
-    await play_lesson(ctx, 1)
+@bot.command(name='Lesson')
+async def lesson(ctx, lesson_number: int):
+    if 1 <= lesson_number <= 30:  # checks that the value is between 1 and 30
+        await play_lesson(ctx, lesson_number)
+    else:
+        await ctx.send('I trust your resolve, but the lesson number should be between 1 and 30.')
 
-@bot.command(name='Lesson02')
-async def lesson02(ctx):
-    await play_lesson(ctx, 2)
-
-@bot.command(name='Lesson03')
-async def lesson03(ctx):
-    await play_lesson(ctx, 3)
-
-@bot.command(name='Lesson04')
-async def lesson04(ctx):
-    await play_lesson(ctx, 4)
-
-@bot.command(name='Lesson05')
-async def lesson05(ctx):
-    await play_lesson(ctx, 5)
-
-@bot.command(name='Lesson06')
-async def lesson06(ctx):
-    await play_lesson(ctx, 6)
-
-@bot.command(name='Lesson07')
-async def lesson07(ctx):
-    await play_lesson(ctx, 7)
-
-@bot.command(name='Lesson08')
-async def lesson08(ctx):
-    await play_lesson(ctx, 8)
-
-@bot.command(name='Lesson09')
-async def lesson09(ctx):
-    await play_lesson(ctx, 9)
-
-@bot.command(name='Lesson10')
-async def lesson10(ctx):
-    await play_lesson(ctx, 10)
-
-@bot.command(name='Lesson11')
-async def lesson11(ctx):
-    await play_lesson(ctx, 11)
-
-@bot.command(name='Lesson12')
-async def lesson12(ctx):
-    await play_lesson(ctx, 12)
-
-@bot.command(name='Lesson13')
-async def lesson13(ctx):
-    await play_lesson(ctx, 13)
-
-@bot.command(name='Lesson14')
-async def lesson14(ctx):
-    await play_lesson(ctx, 14)
-
-@bot.command(name='Lesson15')
-async def lesson15(ctx):
-    await play_lesson(ctx, 15)
-
-@bot.command(name='Lesson16')
-async def lesson16(ctx):
-    await play_lesson(ctx, 16)
-
-@bot.command(name='Lesson17')
-async def lesson17(ctx):
-    await play_lesson(ctx, 17)
-
-@bot.command(name='Lesson18')
-async def lesson18(ctx):
-    await play_lesson(ctx, 18)
-
-@bot.command(name='Lesson19')
-async def lesson19(ctx):
-    await play_lesson(ctx, 19)
-
-@bot.command(name='Lesson20')
-async def lesson20(ctx):
-    await play_lesson(ctx, 20)
-
-@bot.command(name='Lesson21')
-async def lesson21(ctx):
-    await play_lesson(ctx, 21)
-
-@bot.command(name='Lesson22')
-async def lesson22(ctx):
-    await play_lesson(ctx, 22)
-
-@bot.command(name='Lesson23')
-async def lesson23(ctx):
-    await play_lesson(ctx, 23)
-
-@bot.command(name='Lesson24')
-async def lesson24(ctx):
-    await play_lesson(ctx, 24)
-
-@bot.command(name='Lesson25')
-async def lesson25(ctx):
-    await play_lesson(ctx, 25)
-
-@bot.command(name='Lesson26')
-async def lesson26(ctx):
-    await play_lesson(ctx, 26)
-
-@bot.command(name='Lesson27')
-async def lesson27(ctx):
-    await play_lesson(ctx, 27)
-
-@bot.command(name='Lesson28')
-async def lesson28(ctx):
-    await play_lesson(ctx, 28)
-
-@bot.command(name='Lesson29')
-async def lesson29(ctx):
-    await play_lesson(ctx, 29)
-
-@bot.command(name='Lesson30')
-async def lesson30(ctx):
-    await play_lesson(ctx, 30)
-    
 async def _start(ctx):
     user_states[ctx.author.id] = {"state": UserState.CHOOSING_LANGUAGE, "chosen_language": None, "chosen_level": None, "chosen_lesson": None}
 
     free_bot_task = bot.loop.create_task(free_bot())
     await ctx.send('Greetings, ' + ctx.author.mention + '! Choose your path: !Japanese or !Mandarin')
                                           
-                                         
-
 
 
 bot.run(TOKEN)
